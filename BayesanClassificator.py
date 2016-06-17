fcb97706f6f09b3a17e2fa943d155d448886e2a3
Domenico Amato <domen.amato91@gmail.com>
2016-06-17 18:58:11 +0200

    -In BayesanClassificator class        Fixed training Method

--- BayesanClassificator.py
+++ BayesanClassificator.py
@@ -19,20 +19,33 @@ class BayesanClassificator:
     def training(self, trainingSet, classSet, classNum):
 
         apriori_prob= {}
+        prob = {}
         element_in_class = [0]*classNum
+        element_total = 0
+
+        #Calcolo le frequenze di ogni termine nelle classi
         for word in trainingSet:
             apriori_prob[word] = [0]*4
+            prob[word] = 0
             for doc in trainingSet[word]:
+                prob[word] += 1
+                element_total += 1
                 apriori_prob[word][classSet[doc[0]]-1] = + 1
                 element_in_class[classSet[doc[0]]-1] += 1
 
-        
+        #A partire dalle frequenze calcolo le probabilità
         for word in trainingSet:
             cont = 0
+            prob[word] = prob[word]/element_total
             for c in apriori_prob[word]:
                 apriori_prob[word][cont] = c/element_in_class[cont]
                 cont+=1
-        print(apriori_prob)
+
+        self.classificator = {'apriori': apriori_prob, 'prob': prob}
+        print(self.classificator)
+
+        return self.classificator
+
 
     def test(self, testSet):
         pass
